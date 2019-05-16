12da12776fce022225c0af6f25e29561f4e71d7f
Davit Margvelashvili <davit.margvelashvili@gmail.com>
2019-05-16 20:41:36 +0400

    Initialized

---
+++ NumberToText/NumberToText.Tests/NumberToTextTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static NumberToText.NumberToTextConverter;
+
+namespace NumberToText.Tests
+{
+    [TestClass]
+    public class NumberToTextTests
+    {
+        [TestMethod]
+        public void OneDigitNumber()
+        {
+            var expected = "ორი";
+            var actual = Convert(2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TwoDigitNumber()
+        {
+            var expected = "ოცი";
+            var actual = Convert(20);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ThreeDigitNumber()
+        {
+            var expected = "ორას ოცდაერთი";
+            var actual = Convert(221);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FourDigitNumber()
+        {
+            var expected = "ორი ათას სამას ორმოცდახუთი";
+            var actual = Convert(2345);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FiveDigitNumber()
+        {
+            var expected = "ოცდაორი ათას სამას ორმოცდახუთი";
+            var actual = Convert(22345);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SixDigitNumber()
+        {
+            var expected = "სამას ოცდაორი ათას სამას ორმოცდახუთი";
+            var actual = Convert(322345);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ NumberToText/NumberToText/NumberToTextConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToText
+{
+    public static class NumberToTextConverter
+    {
+        static readonly Dictionary<int, string> digits = new Dictionary<int, string>
+        {
+             [1] = "ერთი",
+             [2] = "ორი",
+             [3] = "სამი",
+             [4] = "ოთხი",
+             [5] = "ხუთი",
+             [6] = "ექვსი",
+             [7] = "შვიდი",
+             [8] = "რვა",
+             [9] = "ცხრა",
+        };
+
+        public static string Convert(double number)
+        {
+            int wholePart = (int)number;
+            return digits[wholePart];
+        }
+
+
+    }
+}
