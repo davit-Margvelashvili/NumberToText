f00d5da54cc67d7cc5e512fe4f4fdadbe71441e5
davit.margvelashvili@gmail.com <davit.margvelashvili@gmail.com>
2023-07-10 18:47:52 +0400

    Upgraded to .net7

---
+++ NumberToText/NumberToText.Core.Tests/NumberToTextTests.cs
@@ -0,0 +1,105 @@
+﻿using FluentAssertions;
+using static NumberToText.Core.NumberToTextConverter;
+
+namespace NumberToText.Core.Tests;
+
+public class NumberToTextTests
+{
+    [Fact]
+    public void Overflow()
+    {
+        var action = () => Convert(1000000000000);
+
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void DecimalNumbers()
+    {
+        var (whole, @decimal) = Convert(1234.37);
+        whole.Should().Be("ერთი ათას ორას ოცდათოთხმეტი");
+        @decimal.Should().Be("ოცდაჩვიდმეტი");
+    }
+
+    [Theory]
+    [InlineData(0, "ნული")]
+    [InlineData(1, "ერთი")]
+    [InlineData(2, "ორი")]
+    [InlineData(3, "სამი")]
+    [InlineData(4, "ოთხი")]
+    [InlineData(5, "ხუთი")]
+    [InlineData(6, "ექვსი")]
+    [InlineData(7, "შვიდი")]
+    [InlineData(8, "რვა")]
+    [InlineData(9, "ცხრა")]
+    [InlineData(10, "ათი")]
+    [InlineData(11, "თერთმეტი")]
+    [InlineData(12, "თორმეტი")]
+    [InlineData(13, "ცამეტი")]
+    [InlineData(14, "თოთხმეტი")]
+    [InlineData(15, "თხუთმეტი")]
+    [InlineData(16, "თექვსმეტი")]
+    [InlineData(17, "ჩვიდმეტი")]
+    [InlineData(18, "თვრამეტი")]
+    [InlineData(19, "ცხრამეტი")]
+    [InlineData(20, "ოცი")]
+    [InlineData(21, "ოცდაერთი")]
+    [InlineData(22, "ოცდაორი")]
+    [InlineData(29, "ოცდაცხრა")]
+    [InlineData(30, "ოცდაათი")]
+    [InlineData(31, "ოცდათერთმეტი")]
+    [InlineData(32, "ოცდათორმეტი")]
+    [InlineData(39, "ოცდაცხრამეტი")]
+    [InlineData(40, "ორმოცი")]
+    [InlineData(41, "ორმოცდაერთი")]
+    [InlineData(49, "ორმოცდაცხრა")]
+    [InlineData(50, "ორმოცდაათი")]
+    [InlineData(51, "ორმოცდათერთმეტი")]
+    [InlineData(59, "ორმოცდაცხრამეტი")]
+    [InlineData(60, "სამოცი")]
+    [InlineData(61, "სამოცდაერთი")]
+    [InlineData(69, "სამოცდაცხრა")]
+    [InlineData(70, "სამოცდაათი")]
+    [InlineData(71, "სამოცდათერთმეტი")]
+    [InlineData(79, "სამოცდაცხრამეტი")]
+    [InlineData(80, "ოთხმოცი")]
+    [InlineData(81, "ოთხმოცდაერთი")]
+    [InlineData(89, "ოთხმოცდაცხრა")]
+    [InlineData(90, "ოთხმოცდაათი")]
+    [InlineData(91, "ოთხმოცდათერთმეტი")]
+    [InlineData(99, "ოთხმოცდაცხრამეტი")]
+    [InlineData(100, "ასი")]
+    [InlineData(101, "ას ერთი")]
+    [InlineData(199, "ას ოთხმოცდაცხრამეტი")]
+    [InlineData(200, "ორასი")]
+    [InlineData(299, "ორას ოთხმოცდაცხრამეტი")]
+    [InlineData(300, "სამასი")]
+    [InlineData(399, "სამას ოთხმოცდაცხრამეტი")]
+    [InlineData(900, "ცხრაასი")]
+    [InlineData(999, "ცხრაას ოთხმოცდაცხრამეტი")]
+    [InlineData(1000, "ერთი ათასი")]
+    [InlineData(2000, "ორი ათასი")]
+    [InlineData(10_000, "ათი ათასი")]
+    [InlineData(20_000, "ოცი ათასი")]
+    [InlineData(100_000, "ასი ათასი")]
+    [InlineData(200_000, "ორასი ათასი")]
+    [InlineData(1_000_000, "ერთი მილიონი")]
+    [InlineData(2_000_000, "ორი მილიონი")]
+    [InlineData(10_000_000, "ათი მილიონი")]
+    [InlineData(100_000_000, "ასი მილიონი")]
+    [InlineData(200_000_000, "ორასი მილიონი")]
+    [InlineData(1_000_000_000, "ერთი მილიარდი")]
+    [InlineData(10_000_000_000, "ათი მილიარდი")]
+    [InlineData(100_000_000_000, "ასი მილიარდი")]
+    [InlineData(2_345, "ორი ათას სამას ორმოცდახუთი")]
+    [InlineData(22_345, "ოცდაორი ათას სამას ორმოცდახუთი")]
+    [InlineData(322_345, "სამას ოცდაორი ათას სამას ორმოცდახუთი")]
+    [InlineData(1_322_345, "ერთი მილიონ სამას ოცდაორი ათას სამას ორმოცდახუთი")]
+    [InlineData(21_322_345, "ოცდაერთი მილიონ სამას ოცდაორი ათას სამას ორმოცდახუთი")]
+    [InlineData(321_322_345, "სამას ოცდაერთი მილიონ სამას ოცდაორი ათას სამას ორმოცდახუთი")]
+    [InlineData(7_321_322_345, "შვიდი მილიარდ სამას ოცდაერთი მილიონ სამას ოცდაორი ათას სამას ორმოცდახუთი")]
+    public void RoundNumbers(long number, string result)
+    {
+        Convert(number).wholePart.Should().Be(result);
+    }
+}
---
+++ NumberToText/NumberToText.Core/NumberToTextConverter.cs
@@ -0,0 +1,226 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace NumberToText.Core
+{
+    public static class NumberToTextConverter
+    {
+        #region Fields
+
+        private static readonly Dictionary<byte, string> postfixOnes = new Dictionary<byte, string>
+        {
+            [0] = "",
+            [1] = "ერთი",
+            [2] = "ორი",
+            [3] = "სამი",
+            [4] = "ოთხი",
+            [5] = "ხუთი",
+            [6] = "ექვსი",
+            [7] = "შვიდი",
+            [8] = "რვა",
+            [9] = "ცხრა",
+        };
+
+        private static readonly Dictionary<byte, string> prefixOnes = new Dictionary<byte, string>
+        {
+            [1] = "ერთ",
+            [2] = "ორ",
+            [3] = "სამ",
+            [4] = "ოთხ",
+            [5] = "ხუთ",
+            [6] = "ექვს",
+            [7] = "შვიდ",
+            [8] = "რვა",
+            [9] = "ცხრა",
+        };
+
+        private static readonly Dictionary<byte, string> tens = new Dictionary<byte, string>
+        {
+            [0] = "ათი",
+            [1] = "თერთმეტი",
+            [2] = "თორმეტი",
+            [3] = "ცამეტი",
+            [4] = "თოთხმეტი",
+            [5] = "თხუთმეტი",
+            [6] = "თექვსმეტი",
+            [7] = "ჩვიდმეტი",
+            [8] = "თვრამეტი",
+            [9] = "ცხრამეტი",
+        };
+
+        private static readonly Dictionary<byte, string> prefixTwenties = new Dictionary<byte, string>
+        {
+            [0] = "",
+            [2] = "ოც",
+            [4] = "ორმოც",
+            [6] = "სამოც",
+            [8] = "ოთხმოც",
+        };
+
+        private static readonly Dictionary<byte, string> postfixTwenties = new Dictionary<byte, string>
+        {
+            [0] = "",
+            [1] = "",
+            [2] = "ოცი",
+            [3] = "ოც",
+            [4] = "ორმოცი",
+            [5] = "ორმოც",
+            [6] = "სამოცი",
+            [7] = "სამოც",
+            [8] = "ოთხმოცი",
+            [9] = "ოთხმოც",
+        };
+
+        private static readonly string[] groupNames = new string[]
+        {
+             " ათას ",
+             " მილიონ ",
+             " მილიარდ ",
+        };
+
+        #endregion Fields
+
+        #region Public Methods
+
+        public static (string wholePart, string decimalPart) Convert(double number)
+        {
+            number = Math.Abs(number);
+
+            if (number > 999999999999)
+                throw new ArgumentOutOfRangeException(nameof(number), "Unable to convert. Number was too big");
+
+            var textNumber = $"{number:N}";
+
+            var decimalSeparator = NumberFormatInfo.CurrentInfo.NumberDecimalSeparator;
+            var numberParts = textNumber.Split(new string[] { decimalSeparator }, StringSplitOptions.RemoveEmptyEntries);
+
+            var wholePart = Convert(numberParts[0]);
+            var decimalPart = Convert(numberParts[1]);
+
+            return (wholePart, decimalPart);
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private static string Convert(string number)
+        {
+            if (number.All(d => d == '0')) return "ნული";
+
+            var numberGroups = number.Split(new[] { NumberFormatInfo.CurrentInfo.NumberGroupSeparator }, StringSplitOptions.RemoveEmptyEntries);
+
+            return GroupsToText(numberGroups);
+        }
+
+        public static string GroupsToText(string[] groups)
+        {
+            var numberNames = groups.Select(GroupToText).ToArray();
+
+            var builder = new StringBuilder();
+            for (int i = numberNames.Length - 1, j = 0; i >= 0; i--, j++)
+            {
+                builder.Insert(0, numberNames[i]);
+
+                if (j < numberNames.Length - 1)
+                {
+                    if (numberNames[i - 1] == "")
+                        continue;
+                    builder.Insert(0, groupNames[j]);
+                }
+            }
+            if (numberNames.Last() == "")
+            {
+                builder.Remove(builder.Length - 1, 1);
+                builder.Append("ი");
+            }
+
+            return builder.ToString();
+        }
+
+        public static string GroupToText(string number)
+        {
+            var (first, second, third) = GetDigitsFrom(number);
+
+            string hundred = FindHundred(first);
+            string ten = FindTen(second, third);
+            string one = FindOne(first, second, third);
+
+            return BuildString(first, second, third, hundred, ten, one);
+        }
+
+        private static string BuildString(byte first, byte second, byte third, string hundred, string ten, string one)
+        {
+            var builder = new StringBuilder();
+            builder.Append(hundred == "ერთ" ? "" : hundred);
+            builder.Append(hundred == "" ? "" : "ას ");
+            if (first > 0 && second == 0 && third == 0)
+            {
+                builder.Remove(builder.Length - 1, 1);
+                builder.Append("ი");
+            }
+            else
+            {
+                builder.Append(ten);
+                builder.Append(ten == "" ? "" : one == "" ? "" : "და");
+                builder.Append(ten != "" && one == "" ? "" : one);
+            }
+            return builder.ToString();
+        }
+
+        private static (byte first, byte second, byte third) GetDigitsFrom(string number)
+        {
+            byte first = 0;
+            byte second = 0;
+            byte third = 0;
+            int i = 0;
+
+            if (number.Length > 2)
+            {
+                first = number[i].ToByte();
+                i++;
+            }
+
+            if (number.Length > 1)
+            {
+                second = number[i].ToByte();
+                i++;
+            }
+            third = number[i].ToByte();
+
+            return (first, second, third);
+        }
+
+        private static string FindOne(byte first, byte second, byte third)
+        {
+            if (first == 0 && second == 0) return postfixOnes[third];
+            return second.IsOdd() ? tens[third] : postfixOnes[third];
+        }
+
+        private static string FindTen(byte second, byte third)
+        {
+            if (second == 0) return "";
+            if (third == 0) return postfixTwenties[second];
+            return second.IsOdd() ? prefixTwenties[(byte)(second - 1)] : prefixTwenties[second];
+        }
+
+        private static string FindHundred(byte first)
+        {
+            if (first == 0) return "";
+            return prefixOnes[first];
+        }
+
+        public static byte ToByte(this char digit)
+        {
+            if (char.IsDigit(digit)) return byte.Parse($"{digit}");
+            else throw new FormatException("parameter was not digit");
+        }
+
+        public static bool IsOdd(this byte digit)
+        {
+            return digit % 2 == 1;
+        }
+
+        #endregion Private Methods
+    }
+}
